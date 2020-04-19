09fe46b5686d69672a7ebc7d818c656581f1af9f
Riccardo Rossi <riccardorossi.r2004@outlook.it>
2020-04-19 16:33:47 +0200

    Add project files.

---
+++ Esercizi/Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizi
+{
+    public class Array
+    {
+        public static int[] array(int[] array)
+        {
+
+            int x, y;
+            int temp;
+            for (x = 0; x < array.Length; x++)
+            {
+                for (y = 0; y < array.Length - 1; y++)
+                {
+                    if (array[y] > array[y + 1])
+                    {
+                        temp = array[y];
+                        array[y] = array[y + 1];
+                        array[y + 1] = temp;
+
+                    }
+                }
+
+            }
+            return array;
+        }
+        
+
+
+    }
+}
+                 
+                        
+                    
+
---
+++ Test_esercizi/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_esercizi
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] v1 = new int[6] { 4, 7, 8, 2, 5, 9 };
+            int[] v2 = new int[6] { 2, 4, 5, 7, 8, 9 };
+            int[] ordinato = Esercizi.Array.array(v1);
+            CollectionAssert.AreEquivalent(v2, ordinato);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] v1 = new int[6] { 4, 7, 8, 2, 5, 9 };
+            int[] ordinato = Esercizi.Array.array(v1);
+            Assert.IsNotNull(ordinato);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] v1 = new int[6] { 4, 7, 8, 2, 5, 9 };
+            int lenght = 6;
+            int[] ordinato = Esercizi.Array.array(v1);
+            Assert.IsTrue(ordinato.Length==lenght);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] v1 = new int[6] { a, i, t, s, b, t };
+            int[] v2 = new int[6] { 2, 4, 5, 7, 8, 9 };
+            int[] ordinato = Esercizi.Array.ContainsDoubleChar(v1);
+            CollectionAssert.AreEquivalent(v2, ordinato);
+
+        }
+
+    }
+}
